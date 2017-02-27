16acb763c2e0c1142146a4c75d575c47f6ab37bd
Ihor Korotenko <ihorkorotenko@gmail.com>
2017-02-27 14:36:34 +0200

    Replaced hardcoded client id with ENV config

--- app/authorizers/azure.js
+++ app/authorizers/azure.js
@@ -1,9 +1,10 @@
 import BaseAuthorizer from 'ember-simple-auth/authorizers/base';
 import Ember from 'ember';
 import AuthContext from '../authContext';
+import ENV from '../config/environment';
 export default BaseAuthorizer.extend({
     authorize(sessionData, block){
-        AuthContext.acquireToken('b821c788-c2dd-4665-bb59-a5f90d678844', function(error, token){
+        AuthContext.acquireToken(ENV.authConfigs.clientId, function(error, token){
             block('Authorization', `Bearer ${token}`);    
         });
     }
